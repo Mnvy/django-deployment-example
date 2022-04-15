942b553e1c5ffc84ed7913895201dfcad39fe096
mnvy <manthanvyas36@gmail.com>
2022-04-16 00:22:14 +0530

    'first'

---
+++ learning_templates/basic_app/templatetags/ttags.py
@@ -0,0 +1,10 @@
+from dataclasses import replace
+from django import template
+
+register = template.Library()
+
+@register.filter(name='cut')
+
+def cut(value,arg):
+    return value.replace(arg,'')
+
